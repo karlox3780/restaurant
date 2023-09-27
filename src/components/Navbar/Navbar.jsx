cf549d1aab12d82df0106f881c5187f8aac8f21a
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2023-09-27 21:12:23 +0200

    Añadido el menú lateral

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,10 +3,30 @@ import './Navbar.css';
 
 function Navbar() {
     return (
-        <nav className="fixed flex w-full z-50 items-center justify-between flex-wrap bg-[#b13329] top-0 p-3 animate-fade">
-            <div className="flex items-center flex-shrink-0 text-white mr-6">
+        <nav className="fixed flex flex-col h-full w-[200px] z-50 items-center justify-normal flex-wrap bg-[#24251D] top-0 p-[10px] animate-fade">
+            <div className="flex justify-center flex-shrink-0 text-white">
                 <Link className="text-teal-200 hover:text-white" to="/">
-                    <span className="font-semibold text-[30px] tracking-tight">Restaurant</span>
+                    <span className="font-semibold text-[20px] tracking-tight">Restaurant</span>
+                </Link>
+            </div>
+            <div className="absolute flex flex-col bottom-[10px] flex-shrink-0 text-white">
+                <Link className="text-teal-200 hover:text-white" to="/">
+                    <span className="font-semibold text-[20px] tracking-tight">Inicio</span>
+                </Link>
+                <Link className="text-teal-200 hover:text-white" to="/">
+                    <span className="font-semibold text-[20px] tracking-tight">Carta</span>
+                </Link>
+                <Link className="text-teal-200 hover:text-white" to="/">
+                    <span className="font-semibold text-[20px] tracking-tight">Menus</span>
+                </Link>
+                <Link className="text-teal-200 hover:text-white" to="/">
+                    <span className="font-semibold text-[20px] tracking-tight">Bodega</span>
+                </Link>
+                <Link className="text-teal-200 hover:text-white" to="/">
+                    <span className="font-semibold text-[20px] tracking-tight">Reservas y contactos</span>
+                </Link>
+                <Link className="text-teal-200 hover:text-white" to="/">
+                    <span className="font-semibold text-[20px] tracking-tight">Nosotros</span>
                 </Link>
             </div>
         </nav>

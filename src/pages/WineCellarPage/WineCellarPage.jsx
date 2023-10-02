b9d7512b50a0cbd2e93126da409d498f327db89f
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2023-10-02 22:08:12 +0200

    Creación de json de vinos y implementación básica en la vista de la página

--- src/pages/WineCellarPage/WineCellarPage.jsx
+++ src/pages/WineCellarPage/WineCellarPage.jsx
@@ -1,9 +1,19 @@
 import './WineCellarPage.css';
+import wines from '../../assets/data/wines.json';
 
 function WineCellarPage() {
+    const winesArray = wines;
     return (
         <>
-            Bodega
+            {
+                winesArray.map((wine, index) => {
+                    return (
+                        <div className="text-teal-200" key={index}>
+                            {wine.name}
+                        </div>
+                    )
+                })
+            }
         </>
     )
 }

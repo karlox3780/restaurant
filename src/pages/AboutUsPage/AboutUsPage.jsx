2ade9cd43756523ebbfc8ebacbb0cf00a6a7f96c
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2023-10-10 00:56:39 +0200

    Diseño responsive de la página nosotros

--- src/pages/AboutUsPage/AboutUsPage.jsx
+++ src/pages/AboutUsPage/AboutUsPage.jsx
@@ -4,14 +4,14 @@ import AboutImage from '../../assets/images/About.jpg'
 function AboutUsPage() {
     return (
         <div className='absolute flex flex-col h-fit min-h-full w-full p-[10px] items-center justify-center text-teal-200 animate-fade-down animate-duration-1000'>
-            <img className="w-[300px]" src={AboutImage} alt="Imagen del restaurante" />
-            <p className="w-[500px] pt-[20px] text-justify">
+            <img className="w-[300px] max-md:w-full" src={AboutImage} alt="Imagen del restaurante" />
+            <p className="w-[500px] pt-[20px] text-justify max-md:w-full">
                 Nuestro trato cercano y familiar, junto a nuestra experiencia de años en el sector de la hostelería nos permite elaborar cada día, con ilusión y buen hacer, los mejores platos con los productos más frescos del mercado, para ofrecérselos con la exquisita presencia y cariño que nos caracteriza.
             </p>
-            <p className="w-[500px] text-justify">
+            <p className="w-[500px] text-justify max-md:w-full">
                 La carta, elaborada por profesionales venidos de los fogones del Txistu y el Asador Donostiarra atraen cada día por la calidad de los productos y la maestría con que son preparados.
             </p>
-            <p className="w-[500px] text-justify">
+            <p className="w-[500px] text-justify max-md:w-full">
                 En la plaza ajardinada donde nos encontramos hay un parking subterráneo para dejar el coche. Allí mismo, los niños podrán jugar mientras los padres degustan.
             </p>
         </div>

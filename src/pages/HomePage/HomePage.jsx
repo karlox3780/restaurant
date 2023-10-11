5ce3bbcfeba4cdba9e6eca66fe15d16195fc1a91
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2023-10-11 23:39:37 +0200

    Configurado video para que funcione igual en IOS (Apple)

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,7 +4,7 @@ import video from '../../assets/videos/homeVideo.mp4';
 function HomePage() {
     return (
         <div className='absolute h-full w-full animate-fade-left animate-delay-0 animate-duration-1000'>
-            <video className='h-full w-full object-cover' loop autoPlay muted>
+            <video className='h-full w-full object-cover' playsInline loop autoPlay muted controls={false}>
                 <source src={video} type="video/mp4" />
             </video>
         </div>

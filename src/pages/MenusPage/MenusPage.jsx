8d8c60b2002ee56fc365af3c394b10d1d87e9284
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2023-10-11 23:25:39 +0200

    Añadido diseño responsive en bodega y cambios en los menus

--- src/pages/MenusPage/MenusPage.jsx
+++ src/pages/MenusPage/MenusPage.jsx
@@ -3,9 +3,6 @@ import menus from '../../assets/data/menus.json';
 import FadeInSection from '../../components/FadeInSection/FadeInSection';
 
 function MenusPage() {
-    const { especial_menu } = menus;
-    const { tasting_menu } = menus;
-
     return (
         <div className='absolute flex flex-col items-center justify-center h-fit min-h-full w-full p-[10px] text-teal-200'>
             <FadeInSection class="w-[600px] max-xl:w-full">
@@ -14,73 +11,47 @@ function MenusPage() {
                     <h3>( CONTACTE CON NOSOTROS, EN CASO DE SER UN GRUPO DE MÁS DE 10 PERSONAS )</h3>
                 </div>
             </FadeInSection>
-            <FadeInSection class="w-[600px] max-xl:w-full">
-                <div className='w-[600px] bg-[#24251D] p-[10px] my-[10px] max-xl:w-full'>
-                    <h1 className='text-[25px]'>{especial_menu.name}</h1>
-                    <h1 className='text-[20px] mt-[5px]'>{especial_menu.price_week}</h1>
-                    <h1 className='text-[20px] mt-[5px]'>{especial_menu.price_weekend}</h1>
-                    <div className='border p-[20px] m-[20px]'>
-                        <h2 className='text-[20px]'>{especial_menu.starters.name}</h2>
-                        {
-                            especial_menu.starters.items.map(item => {
-                                return (
-                                    <p>{item}</p>
-                                )
-                            })
-                        }
-                    </div>
-                    <div className='border p-[20px] m-[20px]'>
-                        <h2 className='text-[20px]'>{especial_menu.second_course.name}</h2>
-                        {
-                            especial_menu.second_course.items.map(item => {
-                                return (
-                                    <p>{item}</p>
-                                )
-                            })
-                        }
-                    </div>
-                    <div className='border p-[20px] m-[20px]'>
-                        <h2 className='text-[20px]'>{especial_menu.dessert.name}</h2>
-                        <p>{especial_menu.dessert.item}</p>
-                    </div>
-                    <div className='border p-[20px] m-[20px]'>
-                        <p className=''>{especial_menu.wine}</p>
-                    </div>
+            {
+                menus.map(menu => {
+                    return (
+                        <FadeInSection class="w-[600px] max-xl:w-full">
+                            <div className='w-[600px] bg-[#24251D] p-[10px] my-[10px] max-xl:w-full'>
+                                <h1 className='text-[25px]'>{menu?.name}</h1>
+                                {menu.price_week && <h1 className='text-[20px] mt-[5px]'>{menu.price_week}</h1>}
+                                {menu.price_weekend && <h1 className='text-[20px] mt-[5px]'>{menu.price_weekend}</h1>}
+                                <div className='border p-[20px] m-[20px]'>
+                                    <h2 className='text-[20px]'>{menu?.starters.name}</h2>
+                                    {
+                                        menu?.starters.items.map(item => {
+                                            return (
+                                                <p>{item}</p>
+                                            )
+                                        })
+                                    }
+                                </div>
+                                <div className='border p-[20px] m-[20px]'>
+                                    <h2 className='text-[20px]'>{menu?.second_course.name}</h2>
+                                    {
+                                        menu?.second_course.items.map(item => {
+                                            return (
+                                                <p>{item}</p>
+                                            )
+                                        })
+                                    }
+                                </div>
+                                <div className='border p-[20px] m-[20px]'>
+                                    <h2 className='text-[20px]'>{menu?.dessert.name}</h2>
+                                    <p>{menu?.dessert.item}</p>
+                                </div>
+                                <div className='border p-[20px] m-[20px]'>
+                                    <p className=''>{menu?.drink}</p>
+                                </div>
 
-                </div>
-            </FadeInSection>
-            <FadeInSection class="w-[600px] max-xl:w-full">
-                <div className='w-[600px] bg-[#24251D] p-[10px] mt-[10px] max-xl:w-full'>
-                    <h1 className='text-[25px]'>{tasting_menu.name}</h1>
-                    <div className='border p-[20px] m-[20px]'>
-                        <h2 className='text-[20px]'>{tasting_menu.starters.name}</h2>
-                        {
-                            tasting_menu.starters.items.map(item => {
-                                return (
-                                    <p>{item}</p>
-                                )
-                            })
-                        }
-                    </div>
-                    <div className='border p-[20px] m-[20px]'>
-                        <h2 className='text-[20px]'>{tasting_menu.second_course.name}</h2>
-                        {
-                            tasting_menu.second_course.items.map(item => {
-                                return (
-                                    <p>{item}</p>
-                                )
-                            })
-                        }
-                    </div>
-                    <div className='border p-[20px] m-[20px]'>
-                        <h2 className='text-[20px]'>{tasting_menu.dessert.name}</h2>
-                        <p>{tasting_menu.dessert.item}</p>
-                    </div>
-                    <div className='border p-[20px] m-[20px]'>
-                        <p className=''>{tasting_menu.drink}</p>
-                    </div>
-                </div>
-            </FadeInSection>
+                            </div>
+                        </FadeInSection>
+                    )
+                })
+            }
         </div>
     )
 }
--- src/pages/WineCellarPage/WineCellarPage.jsx
+++ src/pages/WineCellarPage/WineCellarPage.jsx
@@ -13,33 +13,32 @@ function WineCellarPage() {
     return (
         <div className='absolute flex flex-col items-center justify-center h-fit min-h-full w-full p-[10px]'>
             <h1 className='text-[45px] text-[#8ea56f] mb-[20px] animate-fade-down'>Bodega</h1>
-            <div className='w-[70%] grid grid-cols-3'>
-                <div className='text-left mb-[20px] animate-fade-right'>
+            <div className='w-[80%] grid grid-cols-3 max-lg:w-full max-md:grid-cols-1 max-lg:grid-cols-2'>
+                <div className='text-left mb-[20px] animate-fade-right max-lg:animate-fade-down'>
                     <h1 className='text-[25px] text-[#8ea56f]'>Rioja</h1>
                     <WineColum wines={filterWines("Rioja")}></WineColum>
                 </div>
-                <div className='text-left mb-[20px] animate-fade-down'>
+                <div className='text-left mb-[20px] animate-fade-down max-lg:animate-fade-down'>
                     <h1 className='text-[25px] text-[#8ea56f]'>Ribera del Duero</h1>
                     <WineColum wines={filterWines("Ribera del Duero")}></WineColum>
                 </div>
-                <div className='text-left mb-[20px] animate-fade-left'>
+                <div className='text-left mb-[20px] animate-fade-left max-lg:animate-fade-down'>
                     <h1 className='text-[25px] text-[#8ea56f]'>Blancos</h1>
                     <WineColum wines={filterWines("Blancos")}></WineColum>
-
-                    <div className='text-left mt-[20px] animate-fade-left'>
+                    <div className='text-left mt-[20px] animate-fade-left max-lg:animate-fade-down'>
                         <h1 className='text-[25px] text-[#8ea56f]'>Rosados</h1>
                         <WineColum wines={filterWines("Rosados")}></WineColum>
                     </div>
                 </div>
-                <div className='text-left mb-[20px] animate-fade-right'>
+                <div className='text-left mb-[20px] animate-fade-right max-lg:animate-fade-down'>
                     <h1 className='text-[25px] text-[#8ea56f]'>Toro</h1>
                     <WineColum wines={filterWines("Toro")}></WineColum>
                 </div>
-                <div className='text-left mb-[20px] animate-fade-up'>
+                <div className='text-left mb-[20px] animate-fade-up max-lg:animate-fade-down'>
                     <h1 className='text-[25px] text-[#8ea56f]'>Otros</h1>
                     <WineColum wines={filterWines("Otros")}></WineColum>
                 </div>
-                <div className='text-left mb-[20px] animate-fade-left'>
+                <div className='text-left mb-[20px] animate-fade-left max-lg:animate-fade-down'>
                     <h1 className='text-[25px] text-[#8ea56f]'>Cavas</h1>
                     <WineColum wines={filterWines("Cavas")}></WineColum>
                 </div>

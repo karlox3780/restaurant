e4c47e94f136cde14b9154c9e7f8578f24a4a348
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2023-10-15 02:54:40 +0200

    Cambios en el sistema de tabs

--- src/pages/MenuPage/MenuPage.jsx
+++ src/pages/MenuPage/MenuPage.jsx
@@ -1,43 +1,39 @@
-import { Tabs } from 'flowbite-react';
 import './MenuPage.css';
 
 function MenuPage() {
+
+    function handleTabs(e) {
+        e.preventDefault();
+        const root = document.querySelector(".tabs"), tabs = root.querySelectorAll(".tab"), btns = root.querySelectorAll(".btn");
+        var t = e.target, val = t.getAttribute("tab");
+        if (val != null) {
+            tabs.forEach(each => { each.classList.remove("active-tab"); });
+            btns.forEach(each => { each.classList.remove("active-button"); });
+            tabs[val - 1].classList.add("active-tab");
+            btns[val - 1].classList.add("active-button");
+        }
+
+    }
     return (
-        <Tabs.Group
-            aria-label="Pills"
-            style="pills"
-        >
-            <Tabs.Item
-                active
-                title="Tab 1"
-            >
-                <p className="text-sm text-gray-500 dark:text-gray-400">
-                    Content 1
-                </p>
-            </Tabs.Item>
-            <Tabs.Item title="Tab 2">
-                <p className="text-sm text-gray-500 dark:text-gray-400">
-                    Content 2
-                </p>
-            </Tabs.Item>
-            <Tabs.Item title="Tab 3">
-                <p className="text-sm text-gray-500 dark:text-gray-400">
-                    Content 3
-                </p>
-            </Tabs.Item>
-            <Tabs.Item title="Tab 4">
-                <p className="text-sm text-gray-500 dark:text-gray-400">
-                    Content 4
-                </p>
-            </Tabs.Item>
-            <Tabs.Item
-                title="Tab 5"
-            >
-                <p className="text-sm text-gray-500 dark:text-gray-400">
-                    Content 5
-                </p>
-            </Tabs.Item>
-        </Tabs.Group>
+        <div className='absolute flex flex-col h-fit min-h-full w-full p-[10px] items-center justify-center text-teal-200 animate-fade-down animate-duration-1000'>
+            <div class="tabs mx-auto w-[800px]" onClick={handleTabs}>
+                <div class="top flex text-gray-100 overflow-hidden">
+                    <div class="buttons w-full flex bg-[#24251D]">
+                        <span tab="1" class="btn cursor-pointer p-2 px-3 active-button">One</span>
+                        <span tab="2" class="btn cursor-pointer p-2 px-3">Two</span>
+                    </div>
+                </div>
+                <div class="center text-gray-800 relative">
+                    <div class="tab w-full absolute top-0 active-tab">
+
+                    </div>
+
+                    <div class="tab w-full absolute top-0">
+
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 export default MenuPage;

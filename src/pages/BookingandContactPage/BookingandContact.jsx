e86dc6c04f5b4db60be7163f390afa488f26c656
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2023-10-23 23:59:37 +0200

    Añadido titulo y telefono a reservas

--- src/pages/BookingandContactPage/BookingandContact.jsx
+++ src/pages/BookingandContactPage/BookingandContact.jsx
@@ -3,7 +3,8 @@ import './BookingandContact.css';
 function BookingandContactPage() {
     return (
         <div className='absolute h-full w-full animate-fade-left animate-delay-0 animate-duration-1000'>
-
+            <h1>Reservas y Contacto</h1>
+            <span>923 260 246</span>
         </div>
     )
 }

c16b6dace824644b0a8ebe7d1031e8846119eed8
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2023-12-09 23:47:08 +0100

    Cambios en WineColum test

--- src/components/WineColum/WineColum.test.js
+++ src/components/WineColum/WineColum.test.js
@@ -4,7 +4,7 @@ import WineColum from './WineColum';
 
 describe("<WineColum />", () => {
 
-    test("renders the Navbar component", () => {
+    test("renders the WineColum component", () => {
         render(<WineColum />);
     });
 })
